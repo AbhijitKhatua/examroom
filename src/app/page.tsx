124bf4e48952ec5690a31cbd37f6305ffd547a07
AbhijitKhatua <a.k.piintu@gmail.com>
2025-09-21 22:51:04 +0530

    formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,15 +9,15 @@ export default function Home() {
       <div className="flex flex-col items-center">
         <div className="p-4 bg-transparent text-teal-500 rounded-sm w-full xl:w-7xl flex justify-center">
           <div className="border-1 border-teal-500 flex w-full p-4 justify-center rounded-sm">
-          <span className="material-icons text-teal-500">signal_cellular_alt</span>
-          <span className="px-4">{new Date().toLocaleDateString('en-US', {
-            month: 'short',
-            day: 'numeric',
-            year: 'numeric'
-          })}</span>
-          All System Operations
-        </div>
+            <span className="material-icons text-teal-500">signal_cellular_alt</span>
+            <span className="px-4">{new Date().toLocaleDateString('en-US', {
+              month: 'short',
+              day: 'numeric',
+              year: 'numeric'
+            })}</span>
+            All System Operations
           </div>
+        </div>
         <Metrics />
       </div>
     </>
